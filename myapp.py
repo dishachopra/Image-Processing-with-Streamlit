0fcfc229b2deb3168518f4169720d743ef8e3a9a
dishachopra <123184552+dishachopra@users.noreply.github.com>
2023-05-17 02:12:05 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -75,7 +75,7 @@ def photo():
           
         imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
         ret,thresh = cv2.threshold(imgray,y,255,0)
-        image, contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         
         img = cv2.drawContours(im, contours, -1, (0,255,0), 3)
  
